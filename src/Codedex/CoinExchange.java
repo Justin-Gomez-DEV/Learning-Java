f47ebace311846cf1586a54d2bc2a65e6213c60d
Justin-Gomez-DEV <justin.gomez.dev@gmail.com>
2026-06-11 10:36:09 +0100

    Ignored Learning Java.iml

---
+++ src/Codedex/CoinExchange.java
@@ -0,0 +1,15 @@
+package Codedex;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class CoinExchange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        
+
+        scanner.close();
+    }
+}
